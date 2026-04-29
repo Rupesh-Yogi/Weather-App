4d79de8882c96f89d20d6f5d67a3d43baccd2a32
Rupesh Yogi <rupeshyogi712@gmail.com>
2026-04-29 11:27:22 +0100

    changed the way to importing header and footer from external file

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-    // Menu Icon
 
-const menuIcon = document.querySelector(".menu_icon");
-const nav = document.querySelector(".midHeader nav ul");
 
-menuIcon.addEventListener("click", ()=> {
-    nav.classList.toggle("navBar");
-})
+fetch("/components/header.html")
+  .then((res) => res.text())
+  .then((data) => {
+    document.querySelector(".header").innerHTML = data;
 
-    // Dark Mode
+    const menuIcon = document.querySelector(".menu_icon");
+    const nav = document.querySelector(".midHeader nav ul");
+    const darkMode = document.querySelector(".dark_mode");
+    const body = document.querySelector("body");
 
-const darkMode = document.querySelector(".dark_mode");
-const body = document.querySelector("body");
+    menuIcon.addEventListener("click", () => {
+      nav.classList.toggle("navBar");
+    });
 
-darkMode.addEventListener("click", ()=> {
-    body.classList.toggle("dark");
+    darkMode.addEventListener("click", () => {
+      body.classList.toggle("dark");
 
-    if(document.body.classList.contains("dark")) {
+      if (document.body.classList.contains("dark")) {
         localStorage.setItem("theme", "dark");
-    }else{
+      } else {
         localStorage.setItem("theme", "light");
-    }
-})
+      }
+    });
 
-        if (localStorage.getItem("theme") == "dark") {
-            document.body.classList.add("dark");
-        }
+    if (localStorage.getItem("theme") == "dark") {
+      document.body.classList.add("dark");
+    }
+  });
+
+fetch("/components/footer.html")
+  .then((res) => res.text())
+  .then((data) => {
+    document.querySelector(".footer").innerHTML = data;
+  });
