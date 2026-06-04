cc8b04766890ae1f494c268eb1f89edeab8f06d7
Rupesh Yogi <rupeshyogi712@gmail.com>
2026-06-04 22:33:53 +0100

    made form professional that if any of the required field is empty, do not submit

--- form.js
+++ form.js
@@ -3,24 +3,24 @@ const input_of_user = document.querySelectorAll(".userInput");
 const submit_btn = document.querySelector(".formSubmit button");
 
 submit_btn.addEventListener("click", () => {
-    
   let isValid = true;
 
   input_of_user.forEach((element) => {
-    if (element.value.trim() == "") {
-      isValid = false;
+    if (element.hasAttribute("required")) {
+      const errorId = element.getAttribute("data-error-id");
+      const errorMessage = element.getAttribute("data-error");
+      const errorElement = document.querySelector(`#${errorId}`);
+
+      if (element.value.trim() == "") {
+        errorElement.textContent = errorMessage;
+        isValid = false;
+      } else {
+        errorElement.textContent = "";
+      }
     }
   });
 
   if (isValid) {
-    alert("Form submitted successfully"); 
-
-  } else {
-    alert("Please complete the form");
+    alert("Form submitted successfully.");
   }
-
-
-
 });
-
-
